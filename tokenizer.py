617e8f08261e262fd046e8425451184b7e3249ef
Poren Chiang <ren.chiang@gmail.com>
2014-02-22 03:07:30 +0800

    Fix tokenizer indenting. Need to implement CJK tokenizer

--- tokenizer.py
+++ tokenizer.py
@@ -1,10 +1,12 @@
 import re
 
-# Modified from pangu.py 1.0.0 | https://github.com/vinta/pangu.py | MIT License
+URL_LIKE_RE = re.compile(ur'\b[a-z]+:/*[a-z0-9\-\._~:/\?#@!$&\+=]+', flags=re.IGNORECASE)
+
 CJK_CHAR_RANGE = ur'\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff'
 CJK_LEFT_RE = re.compile(u'([' + CJK_CHAR_RANGE + u'])([^' + CJK_CHAR_RANGE + u'\\s])')
 CJK_RIGHT_RE = re.compile(u'([^' + CJK_CHAR_RANGE + u'\\s])([' + CJK_CHAR_RANGE + u'])')
-URL_LIKE_RE = re.compile(ur'\b[a-z]+:/*[a-z0-9\-\._~:/\?#@!$&\+=]+', flags=re.IGNORECASE)
+CJK_SLUG_RE = re.compile(u'([' + CJK_CHAR_RANGE + u']+)')
+
 ASCII_SLUG = ur'[a-z_][a-z0-9\-_\.]*'
 ASCII_SLUG_RE = re.compile(u'[#@]?(' + ASCII_SLUG + u')', flags=re.IGNORECASE)
 
@@ -23,7 +25,25 @@ def preprocess_ascii(text):
 	return text
 
 def preprocess(text):
+	# Ensure unicode string
+	try:
+		text = unicode(text, 'utf8')
+	except TypeError:
+		pass
+
 	text = preprocess_url(text)
 	text = preprocess_cjk(text)
 	text = preprocess_ascii(text)
 	return text
+
+def tokenize(text):
+	tokens = []
+
+	text = preprocess(text)
+	tokens += ASCII_SLUG_RE.findall(text)	# ASCII tokens are already usable
+
+	for unit in CJK_SLUG_RE.findall(text):	# CJK tokens need extraction
+			# TODO: Parse
+			tokens.append(unit)
+
+	return tokens
