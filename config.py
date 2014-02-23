21441f205ed38cc53c388901565bf87e4fbd7053
Poren Chiang <ren.chiang@gmail.com>
2014-02-23 22:00:05 +0800

    Update config file to correct version

--- config.py
+++ config.py
@@ -9,4 +9,5 @@ channel = '#sitcon'
 
 # Setting this to empty string disables syncing
 logging_repo = 'https://github.com/rschiang/monsoon-irc.git'
-logging_author = ''
+logging_name = ''
+logging_email = ''
