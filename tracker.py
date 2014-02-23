4ba84dd03d1f13b34b0c95aa513365506963c42b
Poren Chiang <ren.chiang@gmail.com>
2014-02-23 18:35:47 +0800

    Add internal logging

--- tracker.py
+++ tracker.py
@@ -1,19 +1,30 @@
 from tokenizer import tokenize
+from datetime import datetime
 
 class TrendTracker(object):
 
 	def __init__(self):
 		self.users = {}
 		self.words = {}
+		self.log = []
 		self.initialize()
 
 	def initialize(self):
 		pass
 
 	def track_message(self, user, message):
+		# Append statistics
 		self.users[user] = self.users.get(user, 0) + 1
+		
 		for word in tokenize(message):
 			self.words[word] = self.words.get(word, 0) + 1
+
+		# Append log
+		self.log.append({
+				'at': datetime.utcnow().isoformat(),
+				'by': user,
+				'text': message,
+			})
 
 	def get_user_trend(self, limit=5):
 		return sorted(self.users.keys(), key=lambda x: self.users[x], reverse=True)[:limit]
